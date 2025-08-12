7adb00087125d8d733af368f0edc5ea3e92a2579
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-08-12 11:49:25 +0530

    Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex justify-center my-10'>
+    <div className="card bg-base-300 w-96 shadow-sm">
+    <div className="card-body">
+      <h2 className="card-title justify-center">Login</h2>
+      <fieldset className="fieldset">
+      <legend className="fieldset-legend">What is your email?</legend>
+      <input type="text" className="input" placeholder="Type here" />
+      <legend className="fieldset-legend">What is your password?</legend>
+      <input type="password" className='input' placeholder='Type here' />
+      </fieldset>
+      <div className="card-actions justify-center my-2">
+        <button className="btn btn-primary items-center">Login</button>
+      </div>
+    </div>
+  </div>
+  </div>
   )
 }
 
