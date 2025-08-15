2451cb230abb6b59d16b0d221d27f1b0d009d8c6
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-08-15 22:46:55 +0530

    Feed Page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Base_Url } from '../constants/constants';
+import { addFeed } from '../utils/feedSlice';
+import axios from 'axios';
+import UserCard from './UserCard';
+
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector(store => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    if(feed) return;
+    try {
+      const res = await axios.get(Base_Url + "feed", { withCredentials: true });
+      dispatch(addFeed(res.data));
+    } catch (err) {
+      console.error("Error fetching feed:", err);
+    }
+  };
+  
+
+  useEffect(() => {
+    console.log("useEffect in Feed component ran");
+    getFeed();
+  }, []);
+
+  return (
+    <div className='flex justify-center my-10'>
+      <UserCard />
+    </div>
+  );
+};
+
+export default Feed;
