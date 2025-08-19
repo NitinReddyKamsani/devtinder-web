7d50fa664cb49c3cc2715e029cbba46b0fa380f5
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-08-19 13:45:11 +0530

    connections page

---
+++ src/components/ConnectionCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ConnectionCard = ({users}) => {
+  return (
+    <div className="card bg-base-300 w-80 shadow-sm my-2">
+  <figure>
+    <img className=' w-full h-72 rounded-md'
+      src={users.photo}
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title my-1">{users.firstName + " " + users.lastName}</h2>
+    <p>{users.about}</p>
+    <p>Age : {users.age + " , " + "Gender : " +  users.gender}</p>
+    <p>Skills : {users.skills.join(", ")}</p>
+  </div>
+</div>
+  )
+}
+
+export default ConnectionCard
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,13 +1,19 @@
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { Base_Url } from '../constants/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addConnection } from '../utils/connectionSlice'
+
+import ConnectionCard from './ConnectionCard'
 
 const Connections = () => {
-
+    const dispatch = useDispatch();
+    const connections = useSelector(store=>store.connections);
     const fetchConnections = async ()=>{
         try{
             const res = await axios.get(Base_Url + "user/connections",{withCredentials : true});
             console.log(res?.data?.otherUsers);
+            dispatch(addConnection(res?.data?.otherUsers));
         }
         catch(err){
             console.log(err);
@@ -18,8 +24,19 @@ const Connections = () => {
         fetchConnections();
     },[])
 
+    if(!connections) return;
+    if(connections.length === 0) return <div>No Connections found</div>
+
   return (
-    <div>Connections</div>
+    <>
+    <h1 className='text-2xl my-5 text-center'>Connections</h1>
+    <div className='flex gap-3 mx-2 overflow-y-auto'>
+        
+        {
+            connections.map((connect,index)=> <div key={index} className='flex'><ConnectionCard users={connect}/></div>)
+        }
+    </div>
+    </>
   )
 }
 
