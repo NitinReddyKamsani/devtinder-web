1919815a64c1e602d05f3e031c57b26df8f349be
Nitin Reddy Kamsani <kamsaninitinreddy@gmail.com>
2025-08-29 14:14:46 +0530

    Building real time chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -6,7 +6,8 @@ import { useSelector } from 'react-redux';
 const Chat = () => {
 
   const {targetUserId} = useParams();
-  const [messages,setMessages] = useState([{text : "Hello world"}])
+  const [messages,setMessages] = useState([])
+  const [newMessage, setNewMessage] = useState("");
   const user = useSelector(store => store.user);
   //console.log(user.data.firstName);
   const userId = user?.data?._id
@@ -20,26 +21,36 @@ const Chat = () => {
     socket.emit("joinChat",{userId,targetUserId})
     //console.log(userId,targetUserId)
 
+    socket.on("messageReceived",({userName,text}) => {
+        console.log(userName + " : " + text);
+        setMessages((messages)=>[...messages, {userName,text}])
+    })
+
     return ()=>{
       socket.disconnect();
     }
   },[])
 
+  const sendNewMessage = ()=>{
+    const socket = createSocketConnection();
+    socket.emit("sendMessage",{userName,userId,targetUserId,text:newMessage})
+    setNewMessage("");
+  }
 
   return (
     <div className='w-1/2 mx-auto flex flex-col m-5 border border-gray-600 h-[70vh] '>
       <h1 className='p-5 border border-gray-600  text-2xl text-center'> Chat </h1>
       <div className='flex-1 overflow-scroll p-5 border border-gray-600'>
       {
-        messages.map((map,index)=>{
+        messages.map((msg,index)=>{
           return (
 
               <div key={index} className="chat chat-start">
                 <div className="chat-header">
-                 Nitin
+                 {msg.userName}
                <time className="text-xs opacity-50">2 hours ago</time>
                 </div>
-              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-bubble">{msg.text}</div>
               <div className="chat-footer opacity-50">Seen</div>
               </div>
           )
@@ -47,8 +58,8 @@ const Chat = () => {
        </div>
       <div className='p-5 border-t border-gray-600 flex flex-items justify-center gap-3 '>
         {/* Input boxes */}
-        <input type='text' className="p-2 flex-1 rounded-sm border border-gray-600" placeholder='Type a message' />
-        <button className='btn btn-primary'>Send</button>
+        <input type='text' className="p-2 flex-1 rounded-sm border border-gray-600" placeholder='Type a message' value={newMessage} onChange={(e)=>setNewMessage(e.target.value)} />
+        <button onClick={()=>sendNewMessage("")} className='btn btn-primary'>Send</button>
         </div>
     </div>
   )
